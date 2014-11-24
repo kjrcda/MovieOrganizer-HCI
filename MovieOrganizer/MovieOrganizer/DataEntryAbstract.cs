84675e80c1e34acd6fe30edb561566eca64ce9ac
kjrcda <kjrcda@hotmail.com>
2014-11-24 12:46:01 -0600

    Basic Entry template
    Created basic entry template and tested its linking out

---
+++ MovieOrganizer/MovieOrganizer/DataEntryAbstract.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieOrganizer
+{
+    public partial class DataEntryAbstract : Form
+    {
+        public DataEntryAbstract(String name)
+        {
+            InitializeComponent();
+
+            Text = name;
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Dispose();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Dispose();
+        }
+    }
+}
--- MovieOrganizer/MovieOrganizer/MainForm.Designer.cs
+++ MovieOrganizer/MovieOrganizer/MainForm.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAddMovie = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnAddMovie
+            // 
+            this.btnAddMovie.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMovie.Location = new System.Drawing.Point(302, 243);
+            this.btnAddMovie.Name = "btnAddMovie";
+            this.btnAddMovie.Size = new System.Drawing.Size(110, 41);
+            this.btnAddMovie.TabIndex = 0;
+            this.btnAddMovie.Text = "Add Movie";
+            this.btnAddMovie.UseVisualStyleBackColor = true;
+            this.btnAddMovie.Click += new System.EventHandler(this.btnAddMovie_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnAddMovie);
             this.Name = "MainForm";
             this.Text = "Movie Organizer";
             this.ResumeLayout(false);
@@ -42,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAddMovie;
     }
 }
 
