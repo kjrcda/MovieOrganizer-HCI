054638f2b4c1812062e2e5597bee2fb91075382f
MaddieLvrg <m_lavergne_92@msn.com>
2014-11-26 17:41:49 -0600

    Setting up base forms
    It's not even all of them yet... I just am doing a commit of some really
    light stuff to make sure Github works before doing any heavy-duty work.

---
+++ MovieOrganizer/MovieOrganizer/LibraryForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieOrganizer
+{
+    public partial class LibraryForm : Form
+    {
+        public LibraryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LibraryForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
