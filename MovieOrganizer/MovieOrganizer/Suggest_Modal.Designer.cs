f8e3f555482645187a53509b83ccc45c6d1e189e
kjrcda <kjrcda@hotmail.com>
2014-12-06 21:33:36 -0600

    change SM.designer

--- MovieOrganizer/MovieOrganizer/Suggest_Modal.Designer.cs
+++ MovieOrganizer/MovieOrganizer/Suggest_Modal.Designer.cs
@@ -28,47 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbl_fSuggestion_question = new System.Windows.Forms.Label();
-            this.btn_fSuggestion_Rating = new System.Windows.Forms.Button();
-            this.btn_fSuggestion_Viewed = new System.Windows.Forms.Button();
+            this.lbl_Question = new System.Windows.Forms.Label();
+            this.btn_Rating = new System.Windows.Forms.Button();
+            this.btn_Recent = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lbl_fSuggestion_question
+            // lbl_Question
             // 
-            this.lbl_fSuggestion_question.AutoSize = true;
-            this.lbl_fSuggestion_question.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_fSuggestion_question.Location = new System.Drawing.Point(30, 41);
-            this.lbl_fSuggestion_question.Name = "lbl_fSuggestion_question";
-            this.lbl_fSuggestion_question.Size = new System.Drawing.Size(297, 17);
-            this.lbl_fSuggestion_question.TabIndex = 0;
-            this.lbl_fSuggestion_question.Text = "What type of recommendation would you like?";
+            this.lbl_Question.AutoSize = true;
+            this.lbl_Question.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Question.Location = new System.Drawing.Point(76, 57);
+            this.lbl_Question.Name = "lbl_Question";
+            this.lbl_Question.Size = new System.Drawing.Size(312, 18);
+            this.lbl_Question.TabIndex = 0;
+            this.lbl_Question.Text = "What type of recommendation would you like?";
             // 
-            // btn_fSuggestion_Rating
+            // btn_Rating
             // 
-            this.btn_fSuggestion_Rating.Location = new System.Drawing.Point(33, 71);
-            this.btn_fSuggestion_Rating.Name = "btn_fSuggestion_Rating";
-            this.btn_fSuggestion_Rating.Size = new System.Drawing.Size(118, 23);
-            this.btn_fSuggestion_Rating.TabIndex = 1;
-            this.btn_fSuggestion_Rating.Text = "Rating";
-            this.btn_fSuggestion_Rating.UseVisualStyleBackColor = true;
+            this.btn_Rating.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Rating.Location = new System.Drawing.Point(102, 106);
+            this.btn_Rating.Name = "btn_Rating";
+            this.btn_Rating.Size = new System.Drawing.Size(89, 50);
+            this.btn_Rating.TabIndex = 1;
+            this.btn_Rating.Text = "Rating";
+            this.btn_Rating.UseVisualStyleBackColor = true;
+            this.btn_Rating.Click += new System.EventHandler(this.btn_Rating_Click);
             // 
-            // btn_fSuggestion_Viewed
+            // btn_Recent
             // 
-            this.btn_fSuggestion_Viewed.Location = new System.Drawing.Point(209, 71);
-            this.btn_fSuggestion_Viewed.Name = "btn_fSuggestion_Viewed";
-            this.btn_fSuggestion_Viewed.Size = new System.Drawing.Size(118, 23);
-            this.btn_fSuggestion_Viewed.TabIndex = 2;
-            this.btn_fSuggestion_Viewed.Text = "Recently Viewed";
-            this.btn_fSuggestion_Viewed.UseVisualStyleBackColor = true;
+            this.btn_Recent.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Recent.Location = new System.Drawing.Point(265, 106);
+            this.btn_Recent.Name = "btn_Recent";
+            this.btn_Recent.Size = new System.Drawing.Size(89, 50);
+            this.btn_Recent.TabIndex = 2;
+            this.btn_Recent.Text = "Recently Viewed";
+            this.btn_Recent.UseVisualStyleBackColor = true;
+            this.btn_Recent.Click += new System.EventHandler(this.btn_Recent_Click);
             // 
             // Suggest_Modal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(369, 148);
-            this.Controls.Add(this.btn_fSuggestion_Viewed);
-            this.Controls.Add(this.btn_fSuggestion_Rating);
-            this.Controls.Add(this.lbl_fSuggestion_question);
+            this.ClientSize = new System.Drawing.Size(465, 201);
+            this.Controls.Add(this.btn_Recent);
+            this.Controls.Add(this.btn_Rating);
+            this.Controls.Add(this.lbl_Question);
             this.Name = "Suggest_Modal";
             this.Text = "Suggest A Movie";
             this.ResumeLayout(false);
@@ -78,8 +82,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lbl_fSuggestion_question;
-        private System.Windows.Forms.Button btn_fSuggestion_Rating;
-        private System.Windows.Forms.Button btn_fSuggestion_Viewed;
+        private System.Windows.Forms.Label lbl_Question;
+        private System.Windows.Forms.Button btn_Rating;
+        private System.Windows.Forms.Button btn_Recent;
     }
 }
